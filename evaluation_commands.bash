766fac0eb0cda653f9935254e9ccc38c64af0b16
Neelectric <Neel.R@web.de>
2025-03-22 22:48:08 +0000

    aime and gsm8k

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -6,11 +6,11 @@ MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_mo
 OUTPUT_DIR=data/evals/$MODEL
 
 # # AIME 2024
-# TASK=aime24
-# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-#     --custom-tasks src/open_r1/evaluate.py \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+TASK=aime24
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks src/open_r1/evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 # # MATH-500
 # TASK=math_500
@@ -31,15 +31,15 @@ OUTPUT_DIR=data/evals/$MODEL
 # #     --use-chat-template \
 # #     --output-dir $OUTPUT_DIR 
 
-# # # GSM8k
-# # TASK=leaderboard|gsm8k
-# lighteval vllm $MODEL_ARGS "leaderboard|gsm8k|5|1" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # GSM8k
+# TASK=leaderboard|gsm8k
+lighteval vllm $MODEL_ARGS "leaderboard|gsm8k|5|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 # # MMLU
 # TASK=leaderboard|mmlu
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.75,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
-lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.75,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
+# lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
