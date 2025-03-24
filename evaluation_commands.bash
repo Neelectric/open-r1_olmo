871c9b0ec2970ec14d88b6766c7c8fee6d4bb7f5
Neelectric <Neel.R@web.de>
2025-03-24 10:55:20 +0000

    eval AceInstruct

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -1,7 +1,8 @@
 # MODEL=Neelectric/SmolLM2-1.7B-Instruct_GRPO
 # MODEL=Neelectric/SmolLM2-1.7B-Instruct_GRPO
 # MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFT
-MODEL=CohereForAI/c4ai-command-r7b-12-2024
+# MODEL=CohereForAI/c4ai-command-r7b-12-2024
+MODEL=nvidia/AceInstruct-7B
 NUM_GPUS=3
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
 OUTPUT_DIR=data/evals/$MODEL
@@ -14,11 +15,11 @@ OUTPUT_DIR=data/evals/$MODEL
 #     --output-dir $OUTPUT_DIR
 
 # # # MATH-500
-# # TASK=math_500
-# # lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-# #     --custom-tasks src/open_r1/evaluate.py \
-# #     --use-chat-template \
-# #     --output-dir $OUTPUT_DIR
+TASK=math_500
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks src/open_r1/evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 # # # GPQA Diamond
 # # TASK=gpqa:diamond
