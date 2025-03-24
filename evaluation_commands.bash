715c2fbe80ec37521b88891e99f955418947e4f7
Neelectric <Neel.R@web.de>
2025-03-24 23:59:47 +0000

    more steps

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -6,29 +6,29 @@
 MODEL=Neelectric/SmolLM2-1.7B-Instruct_SFTv00.02
 NUM_GPUS=3
 # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95,}"
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={\"max_new_tokens\":4096,\"temperature\":0.6,\"top_p\":0.95}"
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.9,generation_parameters={\"max_new_tokens\":4096,\"temperature\":0.6,\"top_p\":0.95}"
 OUTPUT_DIR=data/evals/$MODEL
 
 # # # AIME 2024
 TASK=aime24
-lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks src/open_r1/evaluate.py \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#     --custom-tasks src/open_r1/evaluate.py \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
-# # # MATH-500
-TASK=math_500
-lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks src/open_r1/evaluate.py \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# # # # MATH-500
+# TASK=math_500
+# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#     --custom-tasks src/open_r1/evaluate.py \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
-# # # GPQA Diamond
-TASK=gpqa:diamond
-lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks src/open_r1/evaluate.py \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# # # # GPQA Diamond
+# TASK=gpqa:diamond
+# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#     --custom-tasks src/open_r1/evaluate.py \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 # # # # LiveCodeBench
 # # # lighteval vllm $MODEL_ARGS "extended|lcb:codegeneration|0|0" \
