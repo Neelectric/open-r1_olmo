0b12fdaa5a37be444bf5002220ec580621d76e87
Neelectric <Neel.R@web.de>
2025-03-24 20:50:55 +0000

    adding sys prompts

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -4,7 +4,7 @@
 # MODEL=CohereForAI/c4ai-command-r7b-12-2024
 MODEL=nvidia/AceInstruct-7B
 NUM_GPUS=3
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95,system_prompt="You are a helpful AI Assistant that provides well-reasoned and detailed responses. You first think about the reasoning process as an internal monologue and then provide the user with the answer. Respond in the following format: <think>\n...\n</think>\n<answer>\n...\n</answer>"}"
 OUTPUT_DIR=data/evals/$MODEL
 
 # # # AIME 2024
