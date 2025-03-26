4cbdca21a811ada8f8645fcad4be34dc3ede083d
Neelectric <Neel.R@web.de>
2025-03-26 12:22:09 +0000

    Reducing MMLU memory utilization

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -11,25 +11,25 @@ MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_mo
 OUTPUT_DIR=data/evals/$MODEL
 
 # # # AIME 2024
-TASK=aime24
-lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks src/open_r1/evaluate.py \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# TASK=aime24
+# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#     --custom-tasks src/open_r1/evaluate.py \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
-# # # MATH-500
-TASK=math_500
-lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks src/open_r1/evaluate.py \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# # # # MATH-500
+# TASK=math_500
+# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#     --custom-tasks src/open_r1/evaluate.py \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
-# # # GPQA Diamond
-TASK=gpqa:diamond
-lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-    --custom-tasks src/open_r1/evaluate.py \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# # # # GPQA Diamond
+# TASK=gpqa:diamond
+# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+#     --custom-tasks src/open_r1/evaluate.py \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 # # # # LiveCodeBench
 # # # lighteval vllm $MODEL_ARGS "extended|lcb:codegeneration|0|0" \
@@ -44,7 +44,7 @@ lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
 
 # # MMLU
 # TASK=leaderboard|mmlu
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.75,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.6,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
 lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
