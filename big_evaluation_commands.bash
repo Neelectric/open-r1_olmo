f794cf9a6397e3d343a21433a9351cb98bb1d0a3
Neelectric <Neel.R@web.de>
2025-03-29 21:38:00 +0000

    trying

--- big_evaluation_commands.bash
+++ big_evaluation_commands.bash
@@ -1,9 +1,18 @@
-NUM_GPUS=6
+NUM_GPUS=5
 MODEL=Qwen/Qwen2.5-32B
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,tensor_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
 TASK=aime24
 OUTPUT_DIR=data/evals/$MODEL
 
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks src/open_r1/evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR 
+
+
+# # # MATH-500
+TASK=math_500
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
     --custom-tasks src/open_r1/evaluate.py \
