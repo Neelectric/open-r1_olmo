ee36e0d19a76e63feb3484b8e99e6af995477223
Neelectric <Neel.R@web.de>
2025-03-30 22:09:13 +0100

    bumping version to 3, vllm util to 0.7 to avoid OOM on gpu 5, lr to 1e-6, introducing 10 saves per run, lower min lr rate, grad_accum back to 8

--- setup.bash
+++ setup.bash
@@ -10,6 +10,5 @@ uv pip install liger-kernel
 uv pip install huggingface_hub[hf_transfer]
 export HF_HUB_ENABLE_HF_TRANSFER=1
 huggingface-cli download open-r1/OpenR1-Math-220k --repo-type dataset
-huggingface-cli download allenai/OLMo-2-1124-7B-Instruct
 apt install gettext
 pip install gpustat
