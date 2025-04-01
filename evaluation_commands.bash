eab3152725b507c5078dc4746b629311131bc206
Neelectric <Neel.R@web.de>
2025-04-02 00:07:03 +0100

    lets eval!

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -1,10 +1,10 @@
 # MODEL=Neelectric/SmolLM2-1.7B-Instruct_GRPO
 # MODEL=Neelectric/SmolLM2-1.7B-Instruct_GRPO
 # MODEL=Qwen/Qwen2.5-Math-1.5B
-# MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFT
+MODEL=Neelectric/Qwen2.5-7B-Instruct_GRPOv00.05
 # MODEL=CohereForAI/c4ai-command-r7b-12-2024
 # MODEL=nvidia/AceInstruct-7B
-MODEL=Qwen/Qwen2.5-32B
+# MODEL=Qwen/Qwen2.5-32B
 NUM_GPUS=6
 # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95,}"
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.8,generation_parameters={\"max_new_tokens\":4096,\"temperature\":0.6,\"top_p\":0.95}"
@@ -37,10 +37,10 @@ lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
 # # #     --use-chat-template \
 # # #     --output-dir $OUTPUT_DIR 
 
-# # GSM8k
-lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# # # GSM8k
+# lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 # # MMLU
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.5,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
