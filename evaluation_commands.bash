a88ae92ffc8b020e28b6ceb97877cee84acafe9c
Neelectric <Neel.R@web.de>
2025-04-02 14:32:50 +0100

    eval aceinstruct 7b

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -2,9 +2,9 @@
 # MODEL=Neelectric/SmolLM2-1.7B-Instruct_GRPO
 # MODEL=Qwen/Qwen2.5-Math-1.5B
 # MODEL=Neelectric/Qwen2.5-7B-Instruct_GRPOv00.05
-MODEL=Qwen/Qwen2.5-7B-Instruct
+# MODEL=Qwen/Qwen2.5-7B-Instruct
 # MODEL=CohereForAI/c4ai-command-r7b-12-2024
-# MODEL=nvidia/AceInstruct-7B
+MODEL=nvidia/AceInstruct-7B
 # MODEL=Qwen/Qwen2.5-32B
 NUM_GPUS=6
 # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95,}"
