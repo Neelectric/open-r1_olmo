602177b894b52306a7ea898365f35559aa24260a
Neelectric <Neel.R@web.de>
2025-04-13 20:04:39 +0100

    eval on v9

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -6,7 +6,8 @@
 # MODEL=CohereForAI/c4ai-command-r7b-12-2024
 # MODEL=nvidia/AceInstruct-7B
 # MODEL=allenai/OLMo-2-1124-7B-Instruct
-MODEL=Neelectric/Qwen2.5-7B-Instruct_SFTv00.13
+# MODEL=Neelectric/Qwen2.5-7B-Instruct_SFTv00.13
+MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv00.09
 # MODEL=Qwen/Qwen2.5-32B
 NUM_GPUS=8
 # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95,}"
@@ -14,48 +15,48 @@ MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_mo
 # system_prompt="You are a helpful assistant."
 OUTPUT_DIR=data/evals/$MODEL
 
-# # # AIME 2024
-# TASK=aime24
-# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-#     --custom-tasks src/open_r1/evaluate.py \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # AIME 2024
+TASK=aime24
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks src/open_r1/evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
-# # # # MATH-500
-# TASK=math_500
-# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-#     --custom-tasks src/open_r1/evaluate.py \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # # MATH-500
+TASK=math_500
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks src/open_r1/evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
-# # # # GPQA Diamond
-# TASK=gpqa:diamond
-# lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
-#     --custom-tasks src/open_r1/evaluate.py \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # # GPQA Diamond
+TASK=gpqa:diamond
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks src/open_r1/evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 # # # # # LiveCodeBench
 # # # # lighteval vllm $MODEL_ARGS "extended|lcb:codegeneration|0|0" \
 # # # #     --use-chat-template \
 # # # #     --output-dir $OUTPUT_DIR 
 
-# # # ifeval
-# lighteval vllm $MODEL_ARGS "extended|ifeval|0|1" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # ifeval
+lighteval vllm $MODEL_ARGS "extended|ifeval|0|1" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 
-# # # GSM8k
-# lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # GSM8k
+lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
-# # # # MMLU
-# MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
-# lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # # MMLU
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
+lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 
 # MMMLU-Pro 
@@ -65,7 +66,7 @@ accelerate launch -m lm_eval --model hf \
     --tasks leaderboard_mmlu_pro \
     --apply_chat_template \
     --output_path $OUTPUT_DIR \
-    --batch_size auto
+    --batch_size 16
     # --num_processes $NUM_GPUS \
     # --num_machines 1 \
     # --mixed_precision no \
