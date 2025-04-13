aff518b12266e401d91cfc9320e66e2f1bd4e059
Neelectric <Neel.R@web.de>
2025-04-13 15:17:58 +0100

    eval mmlus

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -5,8 +5,8 @@
 # MODEL=Qwen/Qwen2.5-7B-Instruct
 # MODEL=CohereForAI/c4ai-command-r7b-12-2024
 # MODEL=nvidia/AceInstruct-7B
-MODEL=allenai/OLMo-2-1124-7B-Instruct_SFT
-# MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv00.08
+# MODEL=allenai/OLMo-2-1124-7B-Instruct_SFT
+MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv00.08
 # MODEL=Qwen/Qwen2.5-32B
 NUM_GPUS=8
 # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95,}"
@@ -52,10 +52,10 @@ lighteval vllm $MODEL_ARGS "extended|ifeval|0|1" \
 #     --output-dir $OUTPUT_DIR
 
 # # # # MMLU
-# MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
-# lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.85,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
+lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 # MMMLU-Pro 
 # leaderboard_mmlu_pro
