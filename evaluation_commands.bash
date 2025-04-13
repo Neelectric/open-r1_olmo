8cd3f1e73c601502cef9079cb30ae2904bdf46b6
Neelectric <Neel.R@web.de>
2025-04-13 09:04:28 +0100

    move ifeval

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -5,9 +5,9 @@
 # MODEL=Qwen/Qwen2.5-7B-Instruct
 # MODEL=CohereForAI/c4ai-command-r7b-12-2024
 # MODEL=nvidia/AceInstruct-7B
-MODEL=Neelectric/Qwen2.5-7B-Instruct_GRPOv01.01
+MODEL=Neelectric/Qwen2.5-7B-Instruct_SFTv00.13
 # MODEL=Qwen/Qwen2.5-32B
-NUM_GPUS=7
+NUM_GPUS=8
 # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.95,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95,}"
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.8,generation_parameters={\"max_new_tokens\":4096,\"temperature\":0.6,\"top_p\":0.95}"
 # system_prompt="You are a helpful assistant."
@@ -44,13 +44,14 @@ lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
 
-# # ifeval
-lighteval vllm $MODEL_ARGS "lighteval|ifeval|0|1" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
-
 # # # MMLU
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.5,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
 lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
+
+# # ifeval
+lighteval vllm $MODEL_ARGS "lighteval|ifeval|0|1" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
+    
