7836e5a4666fb41e6c16b27ca25c8918976a706a
Neelectric <Neel.R@web.de>
2025-04-14 14:49:07 +0100

    whatevs

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -45,9 +45,9 @@ OUTPUT_DIR=data/evals/$MODEL
 
 
 # # GSM8k
-lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
+lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" 
     # --use-chat-template \
-    --output-dir $OUTPUT_DIR
+    # --output-dir $OUTPUT_DIR
 
 # # # MMLU
 # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
