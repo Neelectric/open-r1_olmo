3322dcb1b1b9adc6f9a02eba1793d8bbce05aa4a
Neelectric <Neel.R@web.de>
2025-04-14 12:50:18 +0100

    no chat template or sys prompt

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -38,22 +38,22 @@ OUTPUT_DIR=data/evals/$MODEL
 # # # #     --use-chat-template \
 # # # #     --output-dir $OUTPUT_DIR 
 
-# # # ifeval
-# lighteval vllm $MODEL_ARGS "extended|ifeval|0|1" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # ifeval
+lighteval vllm $MODEL_ARGS "extended|ifeval|0|1" \
+    # --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 
-# # # GSM8k
-# lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # GSM8k
+lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
+    # --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
-# # # # MMLU
-# MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
-# lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # # MMLU
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
+lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
+    # --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 
 # MMMLU-Pro 
@@ -61,7 +61,7 @@ OUTPUT_DIR=data/evals/$MODEL
 accelerate launch -m lm_eval --model hf \
     --model_args pretrained=$MODEL,dtype=auto, \
     --tasks leaderboard_mmlu_pro \
-    --system_instruction "You are a helpful AI Assistant that provides well-reasoned and detailed responses. You first think about the reasoning process as an internal monologue and then provide the user with the answer. Respond in the following format: <think>\n...\n</think>\n<answer>\n...\n</answer>"\
+    # --apply_chat_template \
     --output_path $OUTPUT_DIR \
     --batch_size 16
     # --num_processes $NUM_GPUS \
