9e8233d286b7496f859d777a97cbf9ecff094e6d
Neelectric <Neel.R@web.de>
2025-04-14 14:26:12 +0100

    trying

--- evaluation_commands.bash
+++ evaluation_commands.bash
@@ -39,32 +39,32 @@ OUTPUT_DIR=data/evals/$MODEL
 # # # #     --output-dir $OUTPUT_DIR 
 
 # # ifeval
-lighteval vllm $MODEL_ARGS "extended|ifeval|0|1" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# lighteval vllm $MODEL_ARGS "extended|ifeval|0|1" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 
 # # GSM8k
 lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|1" \
-    --use-chat-template \
+    # --use-chat-template \
     --output-dir $OUTPUT_DIR
 
 # # # MMLU
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
-lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
+# lighteval vllm $MODEL_ARGS "leaderboard|mmlu|0|1" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 
 # MMMLU-Pro 
 # leaderboard_mmlu_pro
-accelerate launch -m lm_eval --model hf \
-    --model_args pretrained=$MODEL,dtype=auto, \
-    --tasks leaderboard_mmlu_pro \
-    --apply_chat_template \
-    --output_path $OUTPUT_DIR \
-    --batch_size 16
-    # --num_processes $NUM_GPUS \
-    # --num_machines 1 \
-    # --mixed_precision no \
-    # --dynamo_backend no \
+# accelerate launch -m lm_eval --model hf \
+#     --model_args pretrained=$MODEL,dtype=auto, \
+#     --tasks leaderboard_mmlu_pro \
+#     --apply_chat_template \
+#     --output_path $OUTPUT_DIR \
+#     --batch_size 16
+#     # --num_processes $NUM_GPUS \
+#     # --num_machines 1 \
+#     # --mixed_precision no \
+#     # --dynamo_backend no \
