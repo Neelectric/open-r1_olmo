2ba23b1189cd0cbb9f744889950bb90b63b9a2fe
Neelectric <Neel.R@web.de>
2025-04-17 11:17:45 +0100

    re eval Neelectric/OLMo-2-1124-7B-Instruct_GRPOv00.10

--- random_utilities/evaluation_commands.bash
+++ random_utilities/evaluation_commands.bash
@@ -9,26 +9,26 @@ MODEL=Neelectric/OLMo-2-1124-7B-Instruct_GRPOv00.10
 NUM_GPUS=8
 MAX_TOKENS=4096
 # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:$MAX_TOKENS,temperature:0.6,top_p:0.95}"
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=$MAX_TOKENS,max_num_batched_tokens=$MAX_TOKENS,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:$MAX_TOKENS,temperature:0.6,top_p:0.95}"
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$MAX_TOKENS,max_num_batched_tokens=$MAX_TOKENS,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:$MAX_TOKENS,temperature:0.6,top_p:0.95}"
 OUTPUT_DIR=data/evals/$MODEL
 
 # AIME 2024
-TASK=aime24
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# TASK=aime24
+# lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 # MATH-500
-TASK=math_500
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# TASK=math_500
+# lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 # GPQA Diamond
-TASK=gpqa:diamond
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# TASK=gpqa:diamond
+# lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 # LiveCodeBench
 # lighteval vllm $MODEL_ARGS "extended|lcb:codegeneration|0|0" \
