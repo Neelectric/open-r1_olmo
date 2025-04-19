316cd3aa569fe4aca0f1d8a6d81746f13acf957f
Neelectric <Neel.R@web.de>
2025-04-19 14:42:49 +0100

    trying

--- experiments/try_again.bash
+++ experiments/try_again.bash
@@ -1,5 +1,6 @@
-MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=32768,max_num_batched_tokens=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+NUM_GPUS=8
+MODEL=Qwen/Qwen2.5-Math-1.5B
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=32768,max_num_batched_tokens=32768,gpu_memory_utilization=0.8,data_parallel_size=$NUM_GPUS,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
 OUTPUT_DIR=data/evals/$MODEL
 
 # AIME 2024
