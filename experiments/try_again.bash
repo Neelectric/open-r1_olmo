7bdcf9815212a2ec0cf1d75c565e10b196176930
Neelectric <Neel.R@web.de>
2025-04-23 11:31:24 +0100

    NUM_TOKS

--- experiments/try_again.bash
+++ experiments/try_again.bash
@@ -1,6 +1,7 @@
 NUM_GPUS=8
+NUM_TOKS=4096
 MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv00.12
-MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,max_num_batched_tokens=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,max_num_batched_tokens=$NUM_TOKS,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 TASK=aime24
 OUTPUT_DIR=data/evals/$MODEL
 
