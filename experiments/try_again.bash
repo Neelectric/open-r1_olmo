63acd312daff74169147b04e9888c7c9df824b61
Neelectric <Neel.R@web.de>
2025-04-23 11:23:31 +0100

    hmm

--- experiments/try_again.bash
+++ experiments/try_again.bash
@@ -1,22 +1,9 @@
 NUM_GPUS=8
-MODEL=Qwen/Qwen2.5-Math-1.5B
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=32768,max_num_batched_tokens=32768,gpu_memory_utilization=0.8,data_parallel_size=$NUM_GPUS,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv00.12
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,max_num_batched_tokens=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+TASK=aime24
 OUTPUT_DIR=data/evals/$MODEL
 
-# AIME 2024
-TASK=aime24
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
-
-# MATH-500
-TASK=math_500
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
-
-# GPQA Diamond
-TASK=gpqa:diamond
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --use-chat-template \
     --output-dir $OUTPUT_DIR
