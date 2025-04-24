78137f7fc676197ffaead86d108fd4b705b4786f
Neelectric <Neel.R@web.de>
2025-04-24 17:44:06 +0100

    quick mmlu

--- experiments/try_again.bash
+++ experiments/try_again.bash
@@ -41,18 +41,17 @@ OUTPUT_DIR=data/evals/$MODEL
 #     --output-dir $OUTPUT_DIR
 
 # # # # MMLU
-# # MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
-# lighteval vllm $MODEL_ARGS "leaderboard|mmlu|5|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=4096,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
+lighteval vllm $MODEL_ARGS "leaderboard|mmlu|5|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 
-# MMMLU-Pro 
-# leaderboard_mmlu_pro
-accelerate launch -m lm_eval --model hf \
-    --model_args pretrained=$MODEL,dtype=auto, \
-    --tasks leaderboard_mmlu_pro \
-    --output_path $OUTPUT_DIR \
-    --batch_size 16 \
-    --apply_chat_template 
-        # --num_few_shot 5
+## MMMLU-Pro 
+## leaderboard_mmlu_pro
+# accelerate launch -m lm_eval --model hf \
+#     --model_args pretrained=$MODEL,dtype=auto, \
+#     --tasks leaderboard_mmlu_pro \
+#     --output_path $OUTPUT_DIR \
+#     --batch_size 16 \
+#     --apply_chat_template 
