44a814a150f407adfd7960bbcf857dba217a35d5
Neelectric <Neel.R@web.de>
2025-04-25 00:24:49 +0100

    fdsa

--- experiments/try_again.bash
+++ experiments/try_again.bash
@@ -1,6 +1,6 @@
 NUM_GPUS=8
 NUM_TOKS=4096
-MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv01.00
+MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv01.01
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 
 OUTPUT_DIR=data/evals/$MODEL
