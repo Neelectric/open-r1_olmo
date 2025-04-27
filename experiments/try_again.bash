d8ed5b4c08b562a5002af783b4a115e1d483887f
Neelectric <Neel.R@web.de>
2025-04-27 15:15:37 +0100

    eval

--- experiments/try_again.bash
+++ experiments/try_again.bash
@@ -1,33 +1,33 @@
 NUM_GPUS=8
 NUM_TOKS=4096
-MODEL=Neelectric/OLMo-2-1124-7B-Instruct_GRPOv01.03
+MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv01.03
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 
 OUTPUT_DIR=data/evals/$MODEL
 
 
-# AIME 2024
-# TASK=aime24
-# lighteval vllm $MODEL_ARGS "lighteval|aime24|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+### AIME 2024
+TASK=aime24
+lighteval vllm $MODEL_ARGS "lighteval|aime24|0|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
-# # # MATH-500
-# TASK=math_500
-# lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # MATH-500
+TASK=math_500
+lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
-# # GPQA Diamond
-# TASK=gpqa:diamond
-# lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# GPQA Diamond
+TASK=gpqa:diamond
+lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
-##### LiveCodeBench
-#####lighteval vllm $MODEL_ARGS "extended|lcb:codegeneration|0|0" \
-#####    --use-chat-template \
- ####   --output-dir $OUTPUT_DIR 
+#### LiveCodeBench
+####lighteval vllm $MODEL_ARGS "extended|lcb:codegeneration|0|0" \
+####    --use-chat-template \
+ ###   --output-dir $OUTPUT_DIR 
 
 # ifeval
 lighteval vllm $MODEL_ARGS "extended|ifeval|0|0" \
@@ -35,15 +35,15 @@ lighteval vllm $MODEL_ARGS "extended|ifeval|0|0" \
     --output-dir $OUTPUT_DIR
 
 
-# # # GSM8k
-# lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # GSM8k
+lighteval vllm $MODEL_ARGS "lighteval|gsm8k|5|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
-# # # # # MMLU
-# lighteval vllm $MODEL_ARGS "leaderboard|mmlu|5|0" \
-#     --use-chat-template \
-#     --output-dir $OUTPUT_DIR
+# # # # MMLU
+lighteval vllm $MODEL_ARGS "leaderboard|mmlu|5|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
 
 
 # MMMLU-Pro 
