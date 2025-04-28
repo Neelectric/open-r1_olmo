243025b2a777072c71584b1b4b9a2c1a0b23cda3
Neelectric <Neel.R@web.de>
2025-04-28 01:16:57 +0100

    num_fewshot 4

--- experiments/more_math_tasks.bash
+++ experiments/more_math_tasks.bash
@@ -1,7 +1,7 @@
 NUM_GPUS=8
 NUM_TOKS=4096
-# MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv01.04
-MODEL=allenai/OLMo-2-1124-7B-Instruct
+MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv01.04
+# MODEL=allenai/OLMo-2-1124-7B-Instruct
 # MODEL=Neelectric/OLMo-2-1124-7B-Instruct_GRPOv00.10
 MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.92,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 
@@ -24,14 +24,15 @@ OUTPUT_DIR=data/evals/$MODEL
 #     --output-dir $OUTPUT_DIR
 
 #### lighteval|math
-CUDA_VISIBLE_DEVICES=1 lighteval vllm $MODEL_ARGS "lighteval|mathqa|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR
+# CUDA_VISIBLE_DEVICES=1 lighteval vllm $MODEL_ARGS "lighteval|mathqa|0|0" \
+#     --use-chat-template \
+#     --output-dir $OUTPUT_DIR
 
 ## hendrycks_math
 CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 accelerate launch -m lm_eval --model hf \
     --model_args pretrained=$MODEL,dtype=auto, \
     --tasks hendrycks_math \
+    --num_fewshot 4 \
     --output_path $OUTPUT_DIR \
-    --batch_size 16 \
+    --batch_size 64 \
     --apply_chat_template 
