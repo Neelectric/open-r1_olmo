659cf6f5dd52d1570acbb8c4ea8e7c9af0f33af0
Neelectric <Neel.R@web.de>
2025-05-01 11:57:09 +0100

    fdas

--- experiments/try_again.bash
+++ experiments/try_again.bash
@@ -8,9 +8,9 @@ uv pip install vllm==0.7.2
 
 NUM_GPUS=8
 NUM_TOKS=4096
-# MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv01.05
-MODEL=Neelectric/OLMo-2-1124-7B-Instruct_GRPOv01.11
-MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.92,max_num_batched_tokens=4096,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
+MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv01.06
+# MODEL=Neelectric/OLMo-2-1124-7B-Instruct_GRPOv01.11
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=0.9,max_num_batched_tokens=4096,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 
 OUTPUT_DIR=data/evals/$MODEL
 
