5efa987dbf0b2f8828d180b80f5d654f771d2354
Neelectric <Neel.R@web.de>
2025-05-05 11:15:15 +0100

    v6

--- experiments/benchmarks_improved.bash
+++ experiments/benchmarks_improved.bash
@@ -6,7 +6,7 @@ uv pip install vllm==0.7.2
 #  + vllm==0.7.2
 # python3; import nltk; nltk.download('punkt_tab')
 
-NUM_GPUS=7
+NUM_GPUS=8
 NUM_TOKS=4096
 MAX_GPU_USAGE=0.8
 MODEL=Neelectric/OLMo-2-1124-7B-Instruct_SFTv02.05
