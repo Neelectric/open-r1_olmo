4c48374d269995c28272f7fd3dd4862b219ad775
Neelectric <Neel.R@web.de>
2025-05-13 11:00:01 +0100

    various improvements and cleanup

--- experiments/kl_tests.py
+++ experiments/kl_tests.py
@@ -19,39 +19,8 @@ import pandas as pd
 from datasets import Dataset, load_dataset
 import copy
 from pathlib import Path
-
 import math
 
-if torch.backends.mps.is_available():
-    backend = "mps"
-elif torch.cuda.is_available():
-    backend = "cuda" 
-else:
-    backend = "cpu"
-
-
-# def calc_kl(base_logits, ft_model_id, prompts, tokenizer, benchmark_id):
-#     """Process all revisions for a given model"""
-    
-#     revisions = list_revisions(ft_model_id)
-#     print(f"Found {len(revisions)} revisions for {ft_model_id}: {revisions}")
-#     assert len(revisions) == 20   
-    
-#     base_probs = F.softmax(base_logits, dim=-1) # softmaxing in order to be able to compute KL
-    
-#     kls = []
-#     for revision in tqdm(revisions, desc=f"Processing {ft_model_id}"):
-#         pass
-#         print(f"Loading checkpoint model: {ft_model_id}, revision: {revision}")
-#         ft_logits = collect_logits(ft_model_id, prompts, tokenizer, benchmark_id, revision=revision, cache_dir=None, batch_size=10)
-#         kl = F.kl_div(
-#             F.log_softmax(ft_logits, dim=-1),
-#             base_probs,
-#             reduction='batchmean',
-#             log_target=False
-#             )
-#         kls.append(kl)
-#     return kls
 
 def base_vs_ft(base_model, ft_model_id, prompts, tokenizer, benchmark_id, batch_size):
     save_path = "results/" + benchmark_id + "/" + ft_model_id
@@ -85,7 +54,7 @@ def base_vs_ft(base_model, ft_model_id, prompts, tokenizer, benchmark_id, batch_
     max_length = throwaway_input_ids.shape[1]
         
     # let's loop through all revisions one at a time
-    for revision in tqdm(revisions, desc=f"Processing {ft_model_id}"):
+    for revision in tqdm(revisions, desc=f"Processing {ft_model_id}", dynamic_ncols=True):
         print(f"Loading in model {ft_model_id} revision {revision}")
         ft_model = AutoModelForCausalLM.from_pretrained(
                 pretrained_model_name_or_path=ft_model_id,
@@ -107,11 +76,8 @@ def base_vs_ft(base_model, ft_model_id, prompts, tokenizer, benchmark_id, batch_
             with torch.inference_mode():
                 base_outputs = base_model(**base_inputs)
             base_logits = base_outputs.logits.detach().cpu()
-            print(f"base_logits have shape {base_logits.shape}")
             base_probs = F.softmax(base_logits, dim=-1) 
-            print(f"base_probs have shape {base_probs.shape}")
             base_probs_log = torch.log(base_probs)
-            print(f"base_probs_log have shape {base_probs_log.shape}")
             
             ft_inputs = ft_inputs.to(ft_model.device)
             with torch.inference_mode():
@@ -127,18 +93,6 @@ def base_vs_ft(base_model, ft_model_id, prompts, tokenizer, benchmark_id, batch_
                 log_target=True,
             )
             print(f"kl according to F.kl_div = {kl}")
-            # input = F.log_softmax(ft_logits, dim=-1)
-            # target = F.log_softmax(base_logits, dim=-1)
-            # kl_gfg = F.kl_div(
-            #     input,
-            #     target,
-            #     size_average="batchmean",
-            #     log_target=True,
-            # )
-            # print(f"kl according to gfg F.kl_div = {kl}")
-            
-            
-        
             kls_at_rev.append(kl)
         kl_tensor = torch.stack(kls_at_rev, dim=0)
         mean_for_rev = torch.mean(kl_tensor)
@@ -152,13 +106,8 @@ def base_vs_ft(base_model, ft_model_id, prompts, tokenizer, benchmark_id, batch_
     print(kls_dict)
     with open(save_path + f"/kls_{max_prompts}.json", "w") as f:
             json.dump(kls_dict, f)
-    # kls_tensor = torch.stack(kls, dim=0)
-    # torch.save(kls_tensor, save_path + "/kls_tensor.pt")
-    
 
     return kls_dict
-
-
 # torch.distributions.kl.kl_divergence
 
 def prepare_benchmark_prompts(tokenizer, benchmark_id):
@@ -175,55 +124,6 @@ def prepare_benchmark_prompts(tokenizer, benchmark_id):
     templated_ds = conv_ds.map(lambda x: {"formatted_chat": tokenizer.apply_chat_template(x["chat"], tokenize=False, add_generation_prompt=False)}, remove_columns=["chat"])
     templated_list = [elt["formatted_chat"] for elt in templated_ds]
     return templated_list
-
-
-# def collect_logits(model_id, prompts, tokenizer, benchmark_id, revision="main", cache_dir=None, batch_size=10):
-#     """Returns the predictions of a model on a set of prompts."""    
-#     save_path = "results/" + benchmark_id + "/" + model_id
-#     Path(save_path).mkdir(parents=True, exist_ok=True)
-#     try:
-#         all_logits = torch.load(save_path +"/logits.pt", map_location="cpu", weights_only=True)
-#         print(f"Found and loaded logits for {model_id} on {benchmark_id}!")
-#     except:
-#         print(f"Could not load logits for {model_id} on {benchmark_id}, computing them now...")
-#         model = AutoModelForCausalLM.from_pretrained(
-#                 pretrained_model_name_or_path=model_id,
-#                 revision=revision,
-#                 device_map="auto",
-#                 torch_dtype=torch.bfloat16,
-#                 cache_dir=cache_dir,
-#             )
-        
-#         ### before we start this loop, we need to ensure we always pad to the same max size! unfortunately need to tokenize whole dataset first for this
-#         throwaway_input_ids = tokenizer(prompts, return_tensors="pt", padding="longest").input_ids
-#         print(f"our throwaway inputs had size {throwaway_input_ids.shape}")
-#         max_length = throwaway_input_ids.shape[1]
-        
-#         num_prompts = len(prompts)
-#         num_batches = math.ceil(num_prompts/batch_size)
-#         all_logits = []
-        
-#         for i in tqdm(range(num_batches), dynamic_ncols=True):
-#             batch_prompts = prompts[i*batch_size : (i+1)*batch_size]
-#             inputs = tokenizer(batch_prompts, return_tensors="pt", padding="max_length", max_length=max_length).to(model.device)
-#             with torch.inference_mode():
-#                 outputs = model(**inputs)
-#             batch_logits = outputs.logits.detach().cpu()
-#             all_logits.append(batch_logits)
-#             del inputs, outputs
-#             if backend == "cuda":
-#                 torch.cuda.empty_cache()
-#             elif backend == "mps":
-#                 torch.mps.empty_cache()       
-#         logits_tensor = torch.cat(all_logits, dim=0) # going from list of num_batches tensors, to a tensor of shape [num_batches*bsz, num_toks, vocab_size]
-#         torch.save(logits_tensor, save_path + "/logits.pt")
-#         del model
-#         if backend == "cuda":
-#             torch.cuda.empty_cache()
-#         elif backend == "mps":
-#             torch.mps.empty_cache()
-#     return logits_tensor
-
 
 def plot_kls(grpo_model_id, grpo_kls_dict, sft_model_id, sft_kls_dict):
     import matplotlib.pyplot as plt
@@ -297,34 +197,28 @@ def plot_kls(grpo_model_id, grpo_kls_dict, sft_model_id, sft_kls_dict):
 
 def process_all_models():
     """Process and compare all training regimes"""
+    ### params
     base_model_id = "allenai/OLMo-2-1124-7B-Instruct"
     grpo_model_id = "Neelectric/OLMo-2-1124-7B-Instruct_GRPOv01.14"
     sft_model_id = "Neelectric/OLMo-2-1124-7B-Instruct_SFTv02.00"
-    
-    # base_model_id = "google/gemma-2-2b-it"
-    
-    # base_model_id = "HuggingFaceTB/SmolLM2-135M-Instruct"
-    # grpo_model_id = "toastloaf/smollm2-135m-it-orca-agentinstruct-creative"
-    
     benchmark_id = "HuggingFaceH4/MATH-500"
     
+    ### prep tokenizer, base model and prompts
     tokenizer = AutoTokenizer.from_pretrained(grpo_model_id, padding_side="left")    
-    print(f"tokenizer comes from {grpo_model_id}")
     base_model = AutoModelForCausalLM.from_pretrained(
                 pretrained_model_name_or_path=base_model_id,
                 device_map="cuda:0",
                 torch_dtype=torch.bfloat16,
             )
     prompts = prepare_benchmark_prompts(tokenizer, benchmark_id)
+    print(f"tokenizer comes from {grpo_model_id}")
     
-    
+    ### calc kls
     grpo_kls_dict = base_vs_ft(base_model=base_model, ft_model_id=grpo_model_id, prompts=prompts, tokenizer=tokenizer, benchmark_id=benchmark_id, batch_size=1)
     sft_kls_dict = base_vs_ft(base_model=base_model, ft_model_id=sft_model_id, prompts=prompts, tokenizer=tokenizer, benchmark_id=benchmark_id, batch_size=1)
     
+    ### plot kls
     plot_kls(grpo_model_id, grpo_kls_dict, sft_model_id, sft_kls_dict)
-    
-    # compare_training_regimes(training_regimes, bin_edges)
-    
 
 if __name__ == "__main__":
     process_all_models()
