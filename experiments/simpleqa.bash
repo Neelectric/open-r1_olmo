c9aa49870fecd67e133a62979731341e999f6263
Neelectric <Neel.R@web.de>
2025-06-03 19:20:11 +0100

    lets try on meta-llama/Llama-3.1-8B-Instruct

--- experiments/simpleqa.bash
+++ experiments/simpleqa.bash
@@ -1,21 +1,16 @@
-uv pip uninstall transformers
-uv pip uninstall vllm
-uv pip uninstall flash-attn
-uv pip install vllm==0.7.2
-
-
+# uv pip uninstall transformers
+# uv pip uninstall vllm
+# uv pip uninstall flash-attn
+# uv pip install vllm==0.7.2
 
 NUM_GPUS=5
 NUM_TOKS=16384
 MAX_GPU_USAGE=0.8
-MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+MODEL=meta-llama/Llama-3.1-8B-Instruct
 
 REVISION=main
 MODEL_ARGS="model_name=$MODEL,revision=$REVISION,dtype=auto,data_parallel_size=$NUM_GPUS,max_model_length=$NUM_TOKS,gpu_memory_utilization=$MAX_GPU_USAGE,max_num_batched_tokens=$NUM_TOKS,generation_parameters={max_new_tokens:$NUM_TOKS,temperature:0.6,top_p:0.95}"
 OUTPUT_DIR=data/evals/$MODEL
-
-export OPENAI_MAX_PARALLEL_REQUESTS=2  # Reduce from default 10
-export OPENAI_API_RETRY_SLEEP=5        # Increase sleep between retries
 
 ### SimpleQA
 TASK=simpleqa
